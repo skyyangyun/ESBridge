eeae744689a1b69246b02354a82213f80458821f
杨赟 <yangyun@yangyun.name>
2026-08-28 11:45:47 +0800

    :sparkle: 恢复onPageStared函数

--- esbridge/src/main/kotlin/name/yangyun/esbridge/ESBridge.kt
+++ esbridge/src/main/kotlin/name/yangyun/esbridge/ESBridge.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import org.json.JSONArray
 import org.json.JSONObject
 import org.json.JSONTokener
-import kotlin.coroutines.suspendCoroutine
 
 /**
  * 用于注册供JS调用的Android挂起函数
@@ -27,34 +26,22 @@ typealias SyncHandler = (dict: JSONObject) -> JSONObject?
  */
 typealias Callback = (result: Result<JSONObject>) -> Unit
 
-class ESBridge(
-    val webview: WebView,
-    val name: String = "ESBridge",
-    val allowedOriginRules: Set<String> = setOf("*")
-) {
-    private val sync: MutableMap<String, SyncHandler> =  mutableMapOf()
-    private val async: MutableMap<String, Handler> =  mutableMapOf()
-    private val callbacks: MutableMap<String, Callback> = mutableMapOf()
-    private val scope = (webview.context as AppCompatActivity).lifecycleScope
-
-    init {
-        if (!webview.settings.javaScriptEnabled) throw Error("webview.settings.javaScriptEnabled 必须设置为 true")
-        webview.addJavascriptInterface(this, name)
-        val script = $$"""
+private fun buildStartScript(mod: String): String {
+    return $$"""
 // 构建回调
-$$name._callbacks = {} // 用于接受从Android异步返回的数据
-$$name._ci=0n // 回调计数器
-$$name['$list']=()=>JSON.parse($$name._list())
+$$mod._callbacks = {} // 用于接受从Android异步返回的数据
+$$mod._ci=0n // 回调计数器
+$$mod['$list']=()=>JSON.parse($$mod._list())
 
 // 构建函数钩子
-$$name.$hooks = {} // 提供给Android调用函数
-$$name._hi=0n // 钩子计数器
-$$name._promises = {} // 用于异步返回Android的承诺
-$$name._pi=0n // 承诺计数器
+$$mod.$hooks = {} // 提供给Android调用函数
+$$mod._hi=0n // 钩子计数器
+$$mod._promises = {} // 用于异步返回Android的承诺
+$$mod._pi=0n // 承诺计数器
 
 // 构建事件流
-$$name.$emitter = new EventTarget()
-$$name.ESBridgeEvent = class ESBridgeEvent extends CustomEvent{
+$$mod.$emitter = new EventTarget()
+$$mod.ESBridgeEvent = class ESBridgeEvent extends CustomEvent{
 constructor(type, json) {
     super(type, { detail: json });
     Object.defineProperty(this, 'detail', {
@@ -66,7 +53,52 @@ constructor(type, json) {
 }
 }
 """.trimIndent()
-        WebViewCompat.addDocumentStartJavaScript(webview, script, allowedOriginRules)
+}
+
+private fun buildBindScript(mod: String, func: String): String {
+    return """$mod['$func'] = function(dict) {
+const input = JSON.stringify(dict) ?? '{}'
+const output = $mod._call('$func',input)
+return JSON.parse(output)}""".trimIndent()
+}
+
+private fun buildSuspendScript(mod: String, func: String): String {
+    return """$mod['$func'] = async function(dict) {
+const id = 'c' + this._ci++
+return new Promise((resolve, reject) => {
+    const input = JSON.stringify(dict) ?? '{}'
+    this._callbacks[id] = resolve
+    this._suspend('$func', input, id)
+    setTimeout(() => reject('timeout'), 5000)
+}).finally(() => delete this._callbacks[id]).then(JSON.parse)
+}"""
+}
+
+class ESBridge(
+    val webview: WebView,
+    val name: String = "ESBridge",
+    val allowedOriginRules: Set<String> = setOf("*")
+) {
+    private val sync: MutableMap<String, SyncHandler> =  mutableMapOf()
+    private val async: MutableMap<String, Handler> =  mutableMapOf()
+    private val callbacks: MutableMap<String, Callback> = mutableMapOf()
+    private val scope = (webview.context as AppCompatActivity).lifecycleScope
+
+    private val startScript = buildStartScript(name)
+
+    init {
+        if (!webview.settings.javaScriptEnabled) throw Error("webview.settings.javaScriptEnabled 必须设置为 true")
+        webview.addJavascriptInterface(this, name)
+        WebViewCompat.addDocumentStartJavaScript(webview, startScript, allowedOriginRules)
+    }
+
+    /**
+     * 正常情况下无需调用该函数，只有当你在使用 Chrome≤105 或者 使用多窗口 Chrome≤125 的情况下需要在webviewClient中调用
+     */
+    fun onPageStarted() {
+        webview.evaluateJavascript(startScript) {}
+        sync.forEach { (name) -> webview.evaluateJavascript(buildBindScript(this.name, name)) {} }
+        async.forEach { (name) -> webview.evaluateJavascript(buildSuspendScript(this.name, name)) {} }
     }
 
     /**
@@ -135,13 +167,8 @@ constructor(type, json) {
      */
     fun registerCall(name: String, handler: SyncHandler) {
         sync[name] = handler
-        val script = """
-${this.name}['$name'] = function(dict) {
-const input = JSON.stringify(dict) ?? '{}'
-const output = ${this.name}._call('$name',input)
-return JSON.parse(output)};
-""".trimIndent()
-        WebViewCompat.addDocumentStartJavaScript(webview,script, allowedOriginRules)
+        val script = buildBindScript(this.name, name)
+        WebViewCompat.addDocumentStartJavaScript(webview, script, allowedOriginRules)
         webview.evaluateJavascript(script) {}
     }
 //    fun register(name: String, handler: SyncHandler) = registerCall(name, handler)
@@ -151,18 +178,8 @@ return JSON.parse(output)};
      */
     fun registerSuspend(name: String, handler: Handler) {
         async[name] = handler
-        val script = """
-${this.name}['$name'] = async function(dict) {
-const id = 'c' + this._ci++
-return new Promise((resolve, reject) => {
-    const input = JSON.stringify(dict) ?? '{}'
-    this._callbacks[id] = resolve
-    this._suspend('$name', input, id)
-    setTimeout(() => reject('timeout'), 5000)
-}).finally(() => delete this._callbacks[id]).then(JSON.parse)
-}
-        """.trimIndent()
-        WebViewCompat.addDocumentStartJavaScript(webview,script, allowedOriginRules)
+        val script = buildSuspendScript(this.name, name)
+        WebViewCompat.addDocumentStartJavaScript(webview, script, allowedOriginRules)
         webview.evaluateJavascript(script) {}
     }
     fun register(name: String, handler: Handler) = registerSuspend(name, handler)
@@ -183,13 +200,15 @@ return new Promise((resolve, reject) => {
     fun call(name: String, data: JSONObject? = null, callback: Callback) {
         Log.d("ESBridge", "请求JS函数：[$name]$data")
         webview.post {
-            webview.evaluateJavascript("""(() => {
-const result = ${this.name}.${'$'}hooks['$name'](${JSONObject.quote((data ?: "{}").toString())})
+            webview.evaluateJavascript(
+                $$"""(() => {
+const result = $${this.name}.$hooks['$$name']($${JSONObject.quote((data ?: "{}").toString())})
 if(!(result instanceof Promise)) return JSON.stringify(result)
-const id = 'p' + ${this.name}._pi++
-${this.name}._promises[id]=result
+const id = 'p' + $${this.name}._pi++
+$${this.name}._promises[id]=result
 return id
-})()""") { raw ->
+})()"""
+            ) { raw ->
                 Log.d("ESBridge", "call返回结果：$raw")
                 val result = JSONTokener(raw).nextValue() as String
                 if (!result.startsWith("p")) return@evaluateJavascript callback(Result.success(JSONObject(result)))
